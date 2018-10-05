c9c3e9e897e0c73269c807cb970a963b5d743891
Bryan Juma <jumabryan10@gmail.com>
2018-10-05 13:36:12 +0300

    adds profile

--- zone/admin.py
+++ zone/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+# from .models import Profile,Images,tags, Follow,Comments 
+# from .models import Profile,Images,tags, Follow,Comments 
 # Register your models here.
+# class ProfileAdmin(admin.ModelAdmin):
+
+
+# admin.site.register(Profile,)
+# admin.site.register(tags)
+# admin.site.register(Follow)
+# admin.site.register(Comments)
+# admin.site.register(Images)
