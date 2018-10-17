92989a07ffe5a3882a5533c3370c9a80a1c674ea
Bryan Juma <jumabryan10@gmail.com>
2018-10-17 16:49:46 +0300

    profile addded

--- zone/urls.py
+++ zone/urls.py
@@ -1,3 +1,36 @@
+from django.conf.urls import url,include
+from . import views
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    url(r'^$', views.index, name= 'index'),
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate, name='activate'),
+    # url(r'^explore/$', views.explore, name = 'explore'),
+    # url(r'^profile/(\d+)$', views.profile, name = 'profile'),
+    
+    url(r'^post/$', views.new_post, name='new_post'),
+    # url(r'^search/$', views.search_profile, name='search_profile'),
+    # url(r'^add/(\d+)$', views.comment, name='comment'),
+    # url(r'^change_profile/(\d+)$', views.change_profile, name='change_profile'),
+    
+
+]
+# from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls import url
+# from . import views
+
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+
+
+
 # from django.conf.urls import url,include
 # from . import views
 # from django.contrib import admin
@@ -18,17 +51,17 @@
     
 
 # ]
-from django.conf.urls.static import static
-from django.conf import settings
-from django.conf.urls import url
-from . import views
+# from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls import url
+# from . import views
 
-urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+# urlpatterns=[
+#     url('^$',views.welcome,name = 'welcome'),
 
-]
-if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# ]
+# if settings.DEBUG:
+#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 
 
--- zone/views.py
+++ zone/views.py
@@ -1,26 +1,89 @@
-# from django.contrib.auth import login, authenticate
-# from django.contrib.auth.forms import UserCreationForm
-# from django.shortcuts import render, redirect
-# from templates import SignUpForm, NewPostForm, CommentForm, ProfileForm
-# from django.contrib.auth.decorators import login_required
-# from .models import Images, Profile, tags, Comments, Follow
-# import datetime as dt
-# from django.db import transaction
+
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from .forms import SignupForm, NewPostForm, CommentForm, ProfileForm
+from django.contrib.auth.decorators import login_required
+from .models import Images, Profile,Comments
+import datetime as dt
+from django.db import transaction
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
 # Create your views here.
 
 def welcome(request):
     return render(request, 'welcome.html')
 
-# @login_required(login_url='/accounts/login/')
-# def index(request):
-#     user = request.user
-#     images = Images.objects.filter(user_name=request.user)
-#     profile = Profile.objects.filter(user_name=request.user)
-#     avatar = Profile.profile_avatar
-#     includes = {
-#         "user":user,
-#         "images":images,
-#         "profile":profile,
-#     }
-#     return render(request,'ig/index.html', includes)
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your blog account.'
+            message = render_to_string('acc_active_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token':account_activation_token.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                        mail_subject, message, to=[to_email]
+            )
+            email.send()
+            return HttpResponse('Please confirm your email address to complete the registration')
+    else:
+        form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        # return redirect('home')
+        return HttpResponse('Thank you for your email confirmation. <a href="/accounts/login">Click</a>to login your account.')
+    else:
+        return HttpResponse('Activation link is invalid!')
+
+@login_required(login_url='/accounts/login/')
+def index(request):
+    user = request.user
+    images = Images.objects.all()
+    # profile = Profile.objects.filter(user_name=request.user)
+    # avatar = Profile.profile_avatar
+    # includes = {
+    #     "user":user,
+    #     "images":images,
+    #     "profile":profile,
+    # }
+    return render(request,'ig/index.html',{"images": images})
+    
+def new_post(request):
+   current_user = request.user
+   if request.method == 'POST':
+       form =NewPostForm(request.POST,request.FILES)
+       if form.is_valid():
+           image = form.save(commit=False)
+           image.user = current_user
+           image.save()
+       return render(request, 'new_post.html', {"form": form})
+
+   else:
+       form = NewPostForm()
+   return render(request, 'new_post.html', {"form": form})
